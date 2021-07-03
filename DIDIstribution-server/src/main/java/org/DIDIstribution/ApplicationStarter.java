bf09116b53e8b64bbc6e63558a8caec8983b31b2
iamlira <474948760@qq.com>
2021-07-03 10:31:59 +0800

    refeat:mv Starter to ApplicationStarter

--- DIDIstribution-server/src/main/java/org/DIDIstribution/ApplicationStarter.java
+++ DIDIstribution-server/src/main/java/org/DIDIstribution/ApplicationStarter.java
@@ -5,8 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Starter {
+public class ApplicationStarter {
     public static void main(String[] args) {
-        SpringApplication.run(Starter.class,args);
+        SpringApplication.run(ApplicationStarter.class,args);
     }
 }
